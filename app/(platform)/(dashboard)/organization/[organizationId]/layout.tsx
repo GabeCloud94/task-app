96efccfeb0c2ff2d34afefe24bf92aae8320aa00
gabecloud94 <gabecloud94@gmail.com>
2023-11-20 20:44:09 -0700

    Organizations

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -0,0 +1,16 @@
+import { OrgControl } from "./_components/org-control";
+
+const layout = ({
+  children
+}: {
+  children: React.ReactNode;
+}) => {
+  return (
+    <>
+      <OrgControl />
+      {children}
+    </>
+  )
+}
+
+export default layout
